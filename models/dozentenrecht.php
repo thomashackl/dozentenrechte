e6cfed5fbde9e96694e11d81fd2444021286de69
Florian Bieringer <>
2013-11-06 11:26:13 +0100

    View requests

--- models/dozentenrecht.php
+++ models/dozentenrecht.php
@@ -11,10 +11,20 @@
  * @author intelec
  */
 class Dozentenrecht extends SimpleORMap {
+
     public function __construct($id = null) {
         $this->db_table = 'dozentenrechte';
+        $this->has_one['user'] = array(
+            'class_name' => 'User',
+            'foreign_key' => 'for_id'
+        );
+        $this->has_one['institute'] = array(
+            'class_name' => 'Institute',
+            'foreign_key' => 'institute_id'
+        );
         parent::__construct($id);
     }
+
 }
 
 ?>
--- views/show/given.php
+++ views/show/given.php
@@ -1,7 +1,31 @@
 <? if ($rights): ?>
-<table></table>
-    <? foreach ($rights as $right): ?>
-    <? endforeach; ?>
+    <table class="default">
+        <caption>
+            <?= _('Gestellte Dozentenanträge') ?>
+        </caption>
+        <thead>
+            <tr>
+                <th><?= _('Benutzer') ?></th>
+                <th><?= _('Einrichtung') ?></th>
+                <th><?= _('Von') ?></th>
+                <th><?= _('Bis') ?></th>
+                <th><?= _('Status') ?></th>
+                <th><?= _('Aktion') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <? foreach ($rights as $right): ?>
+                <tr>
+                    <td><?= htmlReady($right->user->getFullname()) ?></td>
+                    <td><?= htmlReady($right->institute->name) ?></td>
+                    <td><?= $right->begin ? date('d.m.Y', $right->begin) : _('Unbegrenzt'); ?></td>
+                    <td><?= $right->end == PHP_INT_MAX ? date('d.m.Y', $right->end) : _('Unbegrenzt'); ?></td>
+                    <td><?= $right->verify ? _('Bestätigt') : _('Wartend') ?></td>
+                    <td><?= $right->verify ? : \Studip\Button::create(_('Antrag zurückziehen'), array('reject' => $right->id)) ?></td>
+                </tr>
+            <? endforeach; ?>
+        </tbody>
+    </table>
 <? else: ?>
     <?= _('Von ihnen liegen keine Anträge vor') ?>
 <? endif; ?>
