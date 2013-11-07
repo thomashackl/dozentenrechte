a423f3ede075535df15340141f6e6d16f3854a2e
Florian Bieringer <>
2013-11-07 15:28:45 +0100

    db install update

--- db/install.sql
+++ db/install.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `dozentenrechte` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `from_id` varchar(32) NOT NULL,
-  `for_id` varchar(32) NOT NULL,
-  `institute_id` varchar(32) NOT NULL,
+  `from_id` varchar(32) COLLATE latin1_german1_ci NOT NULL,
+  `for_id` varchar(32) COLLATE latin1_german1_ci NOT NULL,
+  `institute_id` varchar(32) COLLATE latin1_german1_ci NOT NULL,
   `begin` int(11) NOT NULL,
   `end` int(11) NOT NULL,
   `verify` tinyint(4) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
   `mkdate` int(11) NOT NULL,
   `chdate` int(11) NOT NULL,
   PRIMARY KEY (`id`)
