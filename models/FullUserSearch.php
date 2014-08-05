593cd4b7149e228169221681feda0671a76215f2
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-08-05 13:45:57 +0200

    full implementation

--- models/FullUserSearch.php
+++ models/FullUserSearch.php
@@ -17,6 +17,39 @@
 
 class FullUserSearch extends StandardSearch {
 
+    private $search;
+
+    /**
+     *
+     * @param string $search
+     *
+     * @return void
+     */
+    public function __construct($search) {
+        $this->avatarLike = $this->search = $search;
+        $this->sql = $this->getSQL();
+    }
+
+    /**
+     * returns an object of type SQLSearch with parameters to constructor
+     *
+     * @param string $search
+     *
+     * @return SQLSearch
+     */
+    static public function get($search) {
+        return new FullUserSearch($search);
+    }
+
+    /**
+     * returns the title/description of the searchfield
+     *
+     * @return string title/description
+     */
+    public function getTitle() {
+        return _("Nutzer suchen");
+    }
+
     /**
      * returns a sql-string appropriate for the searchtype of the current class
      *
@@ -24,13 +57,19 @@ class FullUserSearch extends StandardSearch {
      */
     private function getSQL() {
         return "SELECT DISTINCT `auth_user_md5`.`user_id`, CONCAT(`auth_user_md5`.`Vorname`, \" \", `auth_user_md5`.`Nachname`, \" (\", `auth_user_md5`.`username`,\")\") " .
-                "FROM `auth_user_md5` LEFT JOIN `user_info` ON (`user_info`.`user_id` = `auth_user_md5`.`user_id`) " .
-                "WHERE (CONCAT(`auth_user_md5`.`Vorname`, \" \", `auth_user_md5`.`Nachname`) LIKE :input " .
-                    "OR (CONCAT(`auth_user_md5`.`Nachname`, \" \", `auth_user_md5`.`Vorname`) LIKE :input " .
-                    "OR `auth_user_md5`.`username` LIKE :input) " .
-                    "AND `auth_user_md5`.`visible` != 'never' " .
-                "ORDER BY `Nachname`, `Vorname`";
+                "FROM `auth_user_md5` LEFT JOIN `user_info ON (user_info.user_id = auth_user_md5.user_id) " .
+                "WHERE (CONCAT(auth_user_md5.Vorname, \" \", auth_user_md5.Nachname) LIKE :input " .
+                    "OR auth_user_md5.username LIKE :input) " .
+                "ORDER BY Vorname, Nachname";
     }
 
+    /**
+     * A very simple overwrite of the same method from SearchType class.
+     * returns the absolute path to this class for autoincluding this class.
+     *
+     * @return: path to this class
+     */
+    public function includePath() {
+        return __file__;
+    }
 }
-
