596d212fcbd6e6a23202ecac4d292632576e3b6a
hackl <thomas.hackl@uni-passau.de>
2017-11-03 12:41:31 +0100

    compatible function signature

--- controllers/show.php
+++ controllers/show.php
@@ -330,7 +330,7 @@ class ShowController extends StudipController {
     }
 
     // customized #url_for for plugins
-    function url_for($to) {
+    function url_for($to = '') {
         $args = func_get_args();
 
         # find params
